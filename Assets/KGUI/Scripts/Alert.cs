cfd8dbf10cd7580984a8809862b0b54ab94b05c2
Kim Ji Yong <xhpichan@gmail.com>
2015-07-02 19:08:00 +0900

    Okay!

---
+++ Assets/KGUI/Scripts/Alert.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Alert : MonoBehaviour {
+	public string AlertTitle = "타이틀 예제";
+
+	[TextArea(3,10)]
+	public string AlertContent = "타오르고 힘차게 우리의 위하여, 뛰노는 실현에 위하여 열락의\n힘있다. 붙잡아 과실이 찾아 찾아다녀도, 투명하되 놀이 구하지\n안고, 있다. 용기가 관현악이며, 얼음이 있다. 영락과 피가 작고\n불러 청춘에서만 있다.";
+
+	public float AlertOut = 5.0f;
+	private float TopSize = 0;
+	private int moved = 0;
+
+	void Start () {
+		Text Title = gameObject.transform.GetChild (0).transform.FindChild ("TextObject").transform.FindChild ("Title").GetComponent<Text> ();
+		Text Content = gameObject.transform.GetChild(0).transform.FindChild("TextObject").transform.FindChild("Content").GetComponent<Text> ();
+
+		if(TopSize == 0)
+			TopSize = gameObject.transform.GetChild (0).transform.FindChild ("Top Panel").transform.localScale.x / AlertOut / 100;
+
+		Title.text = AlertTitle;
+		Content.text = AlertContent;
+
+		StartCoroutine(Dest());
+	}
+
+	void Update () {
+		Start ();
+	}
+
+	IEnumerator Dest() {
+		yield return new WaitForSeconds(0.01f);
+		AlertOut -= 0.01f;
+
+		gameObject.transform.GetChild (0).transform.FindChild ("Top Panel").transform.localScale -= new Vector3(TopSize, 0, 0);
+
+		if(AlertOut <= 0)
+			StartCoroutine(Fade());
+	}
+
+	IEnumerator Fade() {
+		yield return new WaitForSeconds(0.00001f);
+		moved++;
+		gameObject.transform.Translate (new Vector3 (0, 0.3f));
+		gameObject.transform.GetChild (0).GetComponent<CanvasGroup> ().alpha -= 0.10f;
+
+		if(moved > 10)
+			Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/Graphics/FadeGraphics.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeGraphics : MonoBehaviour {
+	public float FadeSpeed = 0.9f;
+	public GameObject FadeObject = null;
+
+	public bool LetStart = false;
+
+	private Image FadeImage = null;
+	private bool ReachClear = false;
+
+	void Start () {
+		FadeImage = FadeObject.transform.GetChild (0).transform.GetComponent<Image>();
+	}
+
+	void Update(){
+		if (LetStart == true)
+			StartFade ();
+	}
+
+	void FadeToClear(){
+		FadeImage.color = Color.Lerp(FadeImage.color, Color.clear, FadeSpeed * Time.deltaTime);
+	}
+	
+	void FadeToBlack(){
+		FadeImage.color = Color.Lerp(FadeImage.color, Color.black, FadeSpeed * Time.deltaTime);
+	}
+
+	void StartFade(){
+		if (ReachClear == false)
+			FadeToClear ();
+		else
+			FadeToBlack ();
+
+		if (FadeImage.color.a <= 0.05f) {
+			ReachClear = true;
+		} else if(FadeImage.color.a >= 0.95f && ReachClear == true){
+			LetStart = false;
+		}
+	}
+}
